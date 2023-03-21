dfefe0b006cbd17afa518d1e64277fd4bbf09bfa
Mikel3010 <mikelgremi30@gmail.com>
2023-03-21 21:18:50 +0100

    fine ese

--- js/app.js
+++ js/app.js
@@ -3,7 +3,17 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            email: ''
+            emails: []
+        }
+    },
+    methods(){
+    },
+    beforeMount(){
+        for (let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                this.emails.push(response.data.response);
+            });
         }
     }
 }).mount('#app')
